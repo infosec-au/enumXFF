e741e3e42dbf9d66aab8eddfa3479f0ee758efa1
shubs <shahshubham369@gmail.com>
2015-07-16 18:44:01 +1000

    Added required arguments

--- enumXFF.py
+++ enumXFF.py
@@ -7,13 +7,13 @@ from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-t", "--target",
-                    help="Restricted Page (target)")
+                    help="Restricted Page (target)", required=True)
 parser.add_argument("-cl", "--badcl",
-                    help="Restricted Access Content Length")
+                    help="Restricted Access Content Length", required=True)
 parser.add_argument("-r", "--range",
-					help="IP range i.e. 0.0.0.0-255.255.255.255")
+					help="IP range i.e. 0.0.0.0-255.255.255.255", required=True)
 parser.add_argument("-w", "--workers",
-					help="Worker/thread count - default is 20", default=20)
+					help="Worker/thread count - default is 100", default=100)
 parser.add_argument("-o", "--output",
 					help="If an IP address returns a higher content length, save IP address to file", 
 						default="working_ips.txt")
@@ -36,11 +36,6 @@ for ip in tqdm(ip_addresses):
 	response = future.result()
 	if response.headers['content-length'] > args.badcl:
 		ip_save_file = open(args.output, "a")
-		ip_save_file.append(args.target + ": " + x_forwarded_for_header + "\n")
-		print "Access granted with {0}".format(ip)
+		ip_save_file.write(args.target + ": " + str(x_forwarded_for_header) + "\n")
+		print "\nAccess granted with {0}".format(ip)
 		break
-
-
-
-
-
